cbe1a41adfe536292c770eaa980ebba04776f05f
Adris3 <advaitris@gmail.com>
2023-09-30 13:02:01 -0400

    changed footer and added padding

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./Footer.style.scss";
 
+const css= "div{padding: 25px, 50px, 75px}";
+
 function Footer() {
-  return <div className="footer">footer</div>;
+
+  <style>(css)</style>
+ 
+  return <div className="footer" >© 2023 Carleton University</div>;
 }
 
 export default Footer;
